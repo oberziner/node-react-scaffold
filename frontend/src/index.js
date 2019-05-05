26de6d2cb06a10972c25a7c4324d99d225cec868
Augusto Oberziner <augustoberziner@gmail.com>
2019-05-05 19:21:37 -0300

    Adding React

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,2 +1,5 @@
-const name = 'World';
-console.log(`Hello ${name}`);
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+ReactDOM.render(<App />, document.getElementById('root'));
